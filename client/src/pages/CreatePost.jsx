6a9c73c5e114970592f5177135d4e8e0d60f801a
andy b <andy.bukovcan@gmail.com>
2023-06-27 16:30:09 -0400

    backend created

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -12,24 +12,48 @@ const CreatePost = () => {
     prompt: '',
     photo: '',
   });
+  
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+  
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+  
+  const handleSurpriseMe = () => {
+    const RandomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: RandomPrompt })
+  }
 
-const generateImage = () => {
+const generateImage = async () => {
+  if(form.prompt) {
+    try {
+      setGeneratingImg(true);
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: form.prompt
+        }),
+      });
 
-}
+      const data = await response.json();
+
+      setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+    }
+  } else {
+    alert('Please enter a prompt')
+  }
+};
 
 const handleSubmit = () => {
 
-}
-
-const handleChange = (e) => {
-  setForm({ ...form, [e.target.name]: e.target.value })
-}
-
-const handleSurpriseMe = () => {
-  const RandomPrompt = getRandomPrompt(form.prompt);
-  setForm({ ...form, prompt: RandomPrompt })
 }
 
   return (
